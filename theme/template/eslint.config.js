58f8077488b128f34f2d776eef0853ad9b68f9df
Talen <talen2004@163.com>
2026-06-23 16:02:48 +0800

    chore: 删除 theme/ 目录

--- theme/template/eslint.config.js
+++
@@ -1,6 +0,0 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-
-export default createConfigForNuxt({
-  // Nuxt 项目配置
-}).append(eslintPluginPrettier);
